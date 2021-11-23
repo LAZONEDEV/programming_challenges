a61f46a07b8ea386ae609f8a50637880a7d663ac
PatFantasia <desouza.paatrice@gmail.com>
2021-11-23 19:33:30 +0100

    Feat : Resolve Flip front challenge

---
+++ challenges/easy/flipfront/resolution.js
@@ -0,0 +1,21 @@
+function flipFront(arr, val){
+    const arrLimit = arr.length;
+    if(val >= arrLimit){
+        val = arrLimit;
+    } else if( val < 2){
+        return arr
+    }
+    const frontArray = arr.slice(0, val);
+    let result = [];
+    const backArray = arr.slice(val);
+    for (let i = val - 1; i >= 0; i--) {
+        result.push(frontArray[i]);
+    }
+    for (let i = 0; i < backArray.length ; i++) {
+         result.push(backArray[i]);
+    }
+    return result
+}
+// const inputArray = [2, 1, 3, 5, 3]
+
+// console.log(flipFront(inputArray, 2));
