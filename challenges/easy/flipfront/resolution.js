9fa1cea87abd5109b18a1c790fc52a6d564a71bf
PatFantasia <desouza.paatrice@gmail.com>
2021-11-25 13:52:52 +0100

    Refactor: Use reverse function instead of for loop

--- challenges/easy/flipfront/resolution.js
+++ challenges/easy/flipfront/resolution.js
@@ -4,18 +4,17 @@ function flipFront(arr, val){
     } else if( val < 2){
         return arr
     }
-    const frontArray = arr.slice(0, val);
-    let result = [];
+    // get front array
+    const result = arr.slice(0, val).reverse()
+    
     const backArray = arr.slice(val);
-    for (let i = val - 1; i >= 0; i--) {
-        result.push(frontArray[i]);
-    }
+
     for (let i = 0; i < backArray.length ; i++) {
          result.push(backArray[i]);
     }
     return result
 }
-// const inputArray = [2, 1, 3, 5, 3]
+const inputArray = [2, 1, 3, 5, 3]
 
-// console.log(flipFront(inputArray, 2));
+console.log(flipFront(inputArray, 2));
 
