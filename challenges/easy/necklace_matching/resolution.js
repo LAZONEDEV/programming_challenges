66119776f7884aed6ae02d7d063914d63664f46e
PatFantasia <desouza.paatrice@gmail.com>
2021-11-25 19:44:51 +0100

    Refactor: Enhance the code

--- challenges/easy/necklace_matching/resolution.js
+++ challenges/easy/necklace_matching/resolution.js
@@ -1,41 +1,34 @@
 
+
 function same_necklace(necklace1, necklace2) {
-    const loopLimit = necklace1.length;
-    const word1 = Array.from(necklace1);
     let word2 = Array.from(necklace2);
-    let result,firstLetter;
+    let firstLetter;
         if( necklace1 === necklace2 ){
-            return result = true;
-        } else if(  necklace1.length !== necklace2.length){
-            
-            return result = false;
+            return true;
+        } 
+         if(  necklace1.length !== necklace2.length){
+            return false;
         }
-        for (let i = 0; i < loopLimit; i++) {
+        for (let i = 0; i < necklace1.length; i++) {
             firstLetter = word2[0];
             word2.splice(0, 1);
             word2.push(firstLetter);
-            if( word1.join("") === word2.join("")){
-                return result = true
-            } else {
-                if( i === loopLimit - 1 ){
-                    result = false;
-                }
-            }
+            if( necklace1 === word2.join("")){
+                return true
+            } 
         }
-    return result 
+    return false 
 }
 
 
  function repeats(necklace) {
-     const limit = necklace.length;
      let arr = Array.from(necklace);
-     const refArr = Array.from(necklace);
      let result = 0;
-        for (let i = 0; i < limit; i++) {
+        for (let i = 0; i < necklace.length; i++) {
             let firstLetter = arr[0];            
             arr.splice(0,1);
             arr.push(firstLetter);
-            if(refArr.join("") === arr.join("")){
+            if(necklace === arr.join("")){
                 result++;
             }
         }
