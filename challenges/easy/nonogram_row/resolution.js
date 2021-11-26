a9010a96f47beb3c8f46577b381cf77d0bb647db
PatFantasia <desouza.paatrice@gmail.com>
2021-11-26 16:24:57 +0100

    Refactor: code optimisation

--- challenges/easy/nonogram_row/resolution.js
+++ challenges/easy/nonogram_row/resolution.js
@@ -4,31 +4,17 @@ function nonogramrow(arr) {
     let prev;
     let result = [];
 
-    const case0 = arr.every(function(elt){
-        return elt === 0;
-    })
-
-    const case1 = arr.every(function(elt){
-        return elt === 1;
-    })
-    if(case0){
-        return result ;
-    }
-    if (case1){
-        return result.push(limit); 
-    }
-
     for (let i = 0; i < limit; i++) {
         prev =  i > 0 ? arr[i - 1] : 0;
         if(arr[i] === 1){
             count++;
-        } else{
+        } else {
             if(prev === 1){
                 result.push(count);
             }
             count = 0;
         }
-        if(arr[i] === 1 && i === limit - 1 ){
+        if(arr[i] === 1 && i === limit - 1 ) {
             result.push(count);
         }
     }
