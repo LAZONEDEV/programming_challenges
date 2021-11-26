764a3504ff89f2bdc3649e6ac20baba9e828180d
PatFantasia <desouza.paatrice@gmail.com>
2021-11-26 10:17:31 +0100

    Refactor: Remove Count variable

--- challenges/easy/making_change/resolution.js
+++ challenges/easy/making_change/resolution.js
@@ -1,22 +1,22 @@
-function change(val){
+function change(val) {
     const arr = [500, 100, 25, 10, 5, 1];
-    let count, rest;
+    let rest;
     let stock = 0;
-    if(val === 0){
+    if(val === 0) {
         return 0
-    }else{
-        for (let i = 0; i < arr.length; i++) {
-            
-            if(val >= arr[i]){
-                rest = val % arr[i];
-                count = parseInt(val / arr[i]);
-                stock += count;
-                val = rest;
-            }
+    } 
+    for (let i = 0; i < arr.length; i++) {
+        
+        if(val >= arr[i]){
+            rest = val % arr[i];
+            stock += parseInt(val / arr[i]);
+            val = rest;
         }
     }
+    
     return stock
 }
 
 // console.log(change(468));
+
 
